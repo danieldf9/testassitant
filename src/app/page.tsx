4682de81c9c361d5457a87308bf7d6516f427c55
Daniel Fernandes <fernandesdaniel.df9@gmail.com>
2025-06-24 15:26:09 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -118,7 +118,7 @@ export default function JiraCaseGenPage() {
           isOpen={isRaiseBugModalOpen}
           onClose={() => setIsRaiseBugModalOpen(false)}
           projectId={selectedProjectId}
-          projectKey={projectKey}
+          projectKey={selectedProjectKey}
           projectName={selectedProjectName}
         />
       )}
